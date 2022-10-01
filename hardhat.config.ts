34bb2c670e7ce2d95da3155cd461111207fbe14c
nikital111 <tvinki56@gmail.com>
2022-10-01 21:02:48 +0300

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,10 @@ const config = {
       chainId: 1337,
     },
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_GOERLI}`,
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
     },
   },
   etherscan: {
